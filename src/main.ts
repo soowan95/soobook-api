fd907ffefe70fdd7e4edaf3aaa0dd371419104ec
soowan95 <ksw9565@gmail.com>
2025-09-01 19:50:00 +0900

    feat: Swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'dev') {
+    const config = new DocumentBuilder()
+      .setTitle(`Soobook API Documentation`)
+      .setDescription(`Soobook API description`)
+      .setVersion('1.0-SNAPSHOT')
+      .addTag('- User', 'User API Documentation')
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+  }
+
   app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3001);
