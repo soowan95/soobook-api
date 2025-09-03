96c0f0741efa60ecd840e5fec2bcb1a2a077ca5b
soowan95 <ksw9565@gmail.com>
2025-09-03 21:22:39 +0900

    chore: Change commit convention.

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-HOOKS_DIR="$(pwd)/hooks"
+HOOKS_DIR="hooks"   # 프로젝트 루트 기준 hooks 디렉토리
 
 if [ -f "$HOOKS_DIR/commit-msg" ]; then
-  if [ -L ".git/hooks/commit-msg" ]; then
-    echo "commit-msg hook already installed. Skipping."
+  TARGET=".git/hooks/commit-msg"
+
+  # 이미 올바른 심볼릭 링크인지 확인
+  if [ "$(readlink -- "$TARGET")" = "../../$HOOKS_DIR/commit-msg" ]; then
+    echo "commit-msg hook already installed and correct. Skipping."
   else
-    ln -sf "$HOOKS_DIR/commit-msg" ".git/hooks/commit-msg"
+    ln -sf "../../$HOOKS_DIR/commit-msg" "$TARGET"
     chmod +x "$HOOKS_DIR/commit-msg"
     echo "Installed commit-msg hook"
   fi
