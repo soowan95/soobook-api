e5c1fbd0bb4f542d49728813ec1d890faf65e729
soowan95 <ksw9565@gmail.com>
2025-10-25 21:13:32 +0900

    feat: make a role decorator #32

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CategoryService } from './category.service';
 import { ResponseMessage } from '../../common/decorators/response-message.decorator';
+import { Role } from '../../common/decorators/user-role.decorator';
+import { UserRole } from '../user/user.entity';
 
 @ApiTags('- Categories')
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @ApiOperation({
+    summary: '[Category] 생성',
+  })
+  @ApiBearerAuth()
+  @ResponseMessage('success.create')
+  @Role(UserRole.ADMIN)
+  @Post('create')
+  async create(): Promise<void> {
+    console.log('category create');
+  }
 
   @ApiOperation({
     summary: '[Category] 전체 조회',
