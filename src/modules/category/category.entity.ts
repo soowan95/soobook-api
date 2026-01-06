1027c9cbdaa13a4588269d9554c9bd93c4b329f5
ksw9565 <ksw9565@inek.co.kr>
2026-01-06 14:43:09 +0900

    hotfix: Remove unnecessary comments

--- src/modules/category/category.entity.ts
+++ src/modules/category/category.entity.ts
@@ -1,4 +1,12 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, Tree, TreeChildren, TreeParent } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  Tree,
+  TreeChildren,
+  TreeParent,
+} from 'typeorm';
 import { Soobook } from '../../common/interfaces/soobook.entity';
 import { Transaction } from '../transaction/transaction.entity';
 import { Recurrence } from '../recurrence/recurrence.entity';
@@ -9,14 +17,9 @@ export class Category extends Soobook {
   @Column({ length: 30, unique: true })
   name: string;
 
-  // @OneToMany(() => Category, (category) => category.parent, {
-  //   cascade: ['remove'],
-  //   onDelete: 'CASCADE',
-  // })
   @TreeChildren()
   children: Category[];
 
-  // @ManyToOne(() => Category, (category) => category.children)
   @TreeParent()
   @JoinColumn({ name: 'parent_id' })
   parent: Category;
